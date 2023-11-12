eede6441b45c878003b4154d9b1c2ed0195b8ad1
ddestrei <ddestrie@gmail.com>
2023-11-12 15:28:33 +0100

    Third commit

--- src/main.py
+++ src/main.py
@@ -1,15 +1,12 @@
-##B#
-#  #
-# ##
-# A##
+import os
 
 
 class State:
 
-    def __init__(self, x, y, char):
+    def __init__(self, x, y, char, states):
         self.x = x
         self.y = y
-        self.states = []
+        self.states = states
         self.states.append(self)
         self.end = False
         self.char = char
@@ -60,25 +57,30 @@ class Frontier:
         y = state.y
         print(x, y)
         board[x][y] = 'A'
+        next_move = 0
         if x + 1 < board_x_size:
             if board[x + 1][y] == ' ' or board[x + 1][y] == 'B':
                 self.states.append(
-                    down(State(x, y, ''), board[x + 1][y]))
+                    down(State(x, y, '', state.states), board[x + 1][y]))
+                next_move += 1
 
         if x - 1 >= 0:
             if board[x - 1][y] == ' ' or board[x - 1][y] == 'B':
                 self.states.append(
-                    up(State(x, y, ''), board[x - 1][y]))
+                    up(State(x, y, '', state.states), board[x - 1][y]))
+                next_move += 1
 
         if y + 1 < board_y_size:
             if board[x][y + 1] == ' ' or board[x][y + 1] == 'B':
                 self.states.append(
-                    right(State(x, y, ''), board[x][y + 1]))
+                    right(State(x, y, '', state.states), board[x][y + 1]))
+                next_move += 1
 
         if y - 1 >= 0:
             if board[x][y - 1] == ' ' or board[x][y - 1] == 'B':
                 self.states.append(
-                    left(State(x, y, ''), board[x][y - 1]))
+                    left(State(x, y, '', state.states), board[x][y - 1]))
+                next_move += 1
 
         self.states.remove(state)
 
@@ -87,39 +89,58 @@ class Frontier:
 
 def read_map():
     board = []
-    file = open("Maps/map3.txt")
+    file = open(os.getcwd().replace("\\src", "\\Maps\\map2.txt"))
     line = file.readline()
+    x_start = 0
+    y_start = 0
+    i = 0
+    j = 0
     while line != '':
         buffor = []
         for c in line:
             if c != '\n':
                 buffor.append(c)
+                if c == 'A':
+                    x_start = i
+                    y_start = j
+            j += 1
+        i += 1
+        j = 0
+
         board.append(buffor)
         line = file.readline()
     file.close()
-    print(board)
-    return board
+    # print(board)
+    return board, x_start, y_start
 
 
 if __name__ == "__main__":
-    board = read_map()
-    state = State(10, 9, 'A')
+    (board, x_start, y_start) = read_map()
+    # print(x_start)
+    # print(y_start)
+    final_board = board
+    board_x_len = board.__len__()
+    board_y_len = board[0].__len__()
+    state = State(x_start, y_start, 'A', [])
     frontier = Frontier(state)
 
-    for b in range(0, 11):
-        for bb in range(0, 11):
+    for b in range(0, board.__len__()):
+        for bb in range(0, board[0].__len__()):
             print(board[b][bb], end='')
         print()
 
     state = frontier.move(board)
     while state is None and frontier.states.__len__() != 0:
-        #for b in range(0, 11):
+        # for b in range(0, 11):
         #    for bb in range(0, 11):
-           #     print(board[b][bb], end='')
-          #  print()
+        #     print(board[b][bb], end='')
+        #  print()
         state = frontier.move(board)
 
-    for b in range(0, 11):
-        for bb in range(0, 11):
-            print(board[b][bb], end='')
+    for s in state.states:
+        final_board[s.x][s.y] = 'A'
+    
+    for b in range(0, board.__len__()):
+        for bb in range(0, board[0].__len__()):
+            print(final_board[b][bb], end='')
         print()
